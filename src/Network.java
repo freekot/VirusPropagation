82dc35664e66537c94c28fa060b40cf6548b0ae2
gaurav38 <grv_srf@yahoo.co.in>
2013-12-03 23:45:04 -0500

    Reading in Graph and creating RealMatix

--- src/Network.java
+++ src/Network.java
@@ -1,16 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
 
 public class Network {
 	private int numberOfNodes;
 	private int numberOfEdges;
-	private int[][] adjacencyMatrix;
+	private RealMatrix adjacencyMatrix;
+	private HashMap<Integer, Node> nodeMap;
 	
-	public Network(String filename){
+	public Network(String filename) throws IOException{
+		nodeMap = new HashMap<Integer, Node>();
 		readNetwork(filename);
-		adjacencyMatrix = new int[numberOfNodes][numberOfNodes];
 	}
 	
-	public void readNetwork(String filename){
-		
+	public void readNetwork(String filename) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String line = br.readLine();
+		String[] tokens = line.split(" ");
+		numberOfNodes = Integer.parseInt(tokens[0]);
+		numberOfEdges = Integer.parseInt(tokens[1]);
+		adjacencyMatrix = new Array2DRowRealMatrix(numberOfNodes, numberOfNodes);
+		int numberOfLines = 0;
+		while((line=br.readLine()) != null) {
+			numberOfLines++;
+			System.out.println("Reading line : "+line);
+			tokens = line.split(" ");
+			int node1 = Integer.parseInt(tokens[0]);
+			int node2 = Integer.parseInt(tokens[1]);
+			adjacencyMatrix.setEntry(node1, node2, 1);
+			adjacencyMatrix.setEntry(node2, node1, 1);
+			Node first;
+			Node second;
+			if(!nodeMap.containsKey(node1)) {
+				first = new Node(node1);
+				nodeMap.put(node1, first);
+			}
+			if(!nodeMap.containsKey(node2)) {
+				second = new Node(node2);
+				nodeMap.put(node2, second);
+			}
+		}
+		System.out.println("Number of edges read = "+numberOfLines);
+		System.out.println("Number of edges ="+numberOfEdges);
+		System.out.println(adjacencyMatrix.getEntry(0, 1));
+		br.close();
 	}
 
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -1,17 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Simulator {
 	
 	static String netFileName;
 	static String inputFileName;
-	int numberOfSteps;
-	double beta;
-	double detla;
+	static int numberOfSteps;
+	static double beta;
+	static double delta;
+	static double numberOfVaccines;
+	private static BufferedReader in;
 	
-
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//input.txt is the only argument and should contain
 		// 1. input of network file name
@@ -19,17 +24,38 @@ public class Simulator {
 		// 3. to run simulation?
 		// 4. the number of time steps
 		inputFileName = args[0];
-
+		in = new BufferedReader(new FileReader(inputFileName));
 		readInput(inputFileName);
 		
-		
+		//Populate the Adjacency Matrix and create "Nodes"
 		Network net = new Network (netFileName);
-		runSimulation(net);
+		
+		//runSimulation(net);
 
 	}
 	
-	private static void readInput(String inputFileName){
+	private static void readInput(String inputFileName) throws IOException {
 		
+		//Read in the static.network file name
+		String line = in.readLine();
+		String[] tokens = line.split(" ");
+		netFileName = tokens[2];
+		System.out.println(netFileName);
+		
+		//Read in transmission and healing probabilities
+		line = in.readLine();
+		tokens = line.split(" ");
+		beta = Double.parseDouble(tokens[2]);
+		line = in.readLine();
+		tokens = line.split(" ");
+		delta = Double.parseDouble(tokens[2]);	
+		
+		//Read in the number of vaccines
+		line = in.readLine();
+		tokens = line.split(" ");
+		numberOfVaccines = Double.parseDouble(tokens[2]);
+		
+		in.close();
 	}
 	
 	private static void runSimulation(Network net){
